cb6da6be9b962d445c90e76c7c31c742c5486ac0
aspyo <sungpyo8334@naver.com>
2024-06-10 02:19:31 +0900

    FEAT: 공유 기능 추가
    기사 상세보기 화면에서 기사 공유하기 기능 구현

--- app/src/main/java/com/example/newnewss/activity/NewsDetailActivity.java
+++ app/src/main/java/com/example/newnewss/activity/NewsDetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.newnewss.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +15,9 @@ import androidx.core.text.HtmlCompat;
 import com.example.newnewss.R;
 
 public class NewsDetailActivity extends AppCompatActivity {
+    private TextView titleTextView;
+    private TextView descriptionTextView;
+    private Button shareButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +27,7 @@ public class NewsDetailActivity extends AppCompatActivity {
         TextView titleTextView = findViewById(R.id.titleTextView);
         TextView descriptionTextView = findViewById(R.id.descriptionTextView);
         TextView linkTextView = findViewById(R.id.linkTextView);
+        shareButton = findViewById(R.id.shareButton);
 
         // Intent로부터 데이터 가져오기
         String title = getIntent().getStringExtra("title");
@@ -34,5 +41,16 @@ public class NewsDetailActivity extends AppCompatActivity {
         String linkText = "<a href=\"" + link + "\">" + link + "</a>";
         linkTextView.setText(HtmlCompat.fromHtml(linkText, HtmlCompat.FROM_HTML_MODE_LEGACY));
         linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, title + "\n" + description + "\n" + link);
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "공유"));
+            }
+        });
     }
 }
